3bfaaa759cd287df2dfb786495f6734e3991962d
abhishiek GH <abhishekghaveri@gmail.com>
2024-08-31 23:50:40 +0200

    trying to commit in gitbas after adding .ignore  file

---
+++ selenium01.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+
+#create session
+#send the commands 
+# if your using selenium we use to set thedriver path if > 4 they will handle automatically 
+
+browser= webdriver.Chrome()
+browser.get("https://app.vwo.com/")
+
